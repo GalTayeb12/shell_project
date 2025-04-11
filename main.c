1d28a6a1e6fe00eec9a214f9f558e7034a7d4406
Noamda5 <noamda5@ac.sce.ac.il>
2025-04-11 14:55:49 +0300

    main change

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main (int argc, char **argv) {
         } 
         else {  
             /* parent code */ 
-            waitpid(childPid);          
+            waitpid(childPid);       
         }   
     }  
 } 
